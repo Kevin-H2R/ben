313a0b492bd1ff2d6426edec551c7bebf7fd3f26
Kevin <herrbach.kevin@gmail.com>
2024-01-28 20:57:26 +0900

    Adds init db script + adds sign up endpoint

--- ApiGateway/index.js
+++ ApiGateway/index.js
@@ -4,8 +4,35 @@ const app = express()
 const port = 3000
 require('dotenv').config()
 
-app.get('/', async (req, res) => {
-  const response = await axios.get(`http://${process.env.AUTH_SERVICE_NAME}:${process.env.AUTH_SERVICE_PORT}/`)
+// `http://${process.env.AUTH_SERVICE_NAME}:${process.env.AUTH_SERVICE_PORT}/`
+
+app.post('/api/signup', async (req, res) => {
+  console.log("HERE")
+  try {
+    const response = await axios.post(
+      `http://${process.env.AUTH_SERVICE_NAME}:${process.env.AUTH_SERVICE_PORT}/signup`,
+      req,
+      {
+        headers: {
+        // Overwrite Axios's automatically set Content-Type
+        'Content-Type': 'application/json'
+        }
+      }
+    )
+    console.log(response)
+    res.json(response.data)
+  } catch (e) {
+    console.log(e)
+    res.status(500).json(e)
+  }
+})
+
+app.get('', (req, res) => {
+  res.json("GOOD GOOD GOOD")
+})
+
+app.post('/api/signin', async (req, res) => {
+  const response = await axios.get()
   res.json(response.data)
 })
 
--- AuthService/index.js
+++ AuthService/index.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+const pgp = require('pg-promise')()
+
 const app = express()
 const port = 3000
+const db = pgp('postgres://postgres:postgres@db-as:5432/as_db')
 
-app.get('/', (req, res) => {
-  res.send('Hello from auth service')
+app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.one(
+      'INSERT INTO users (email, username, password) VALUES ($1, $2, $3)', [email, username, hashedPassword])
+    res.json("good")
+  } catch (e) {
+    res.json(e.message)
+  }
 })
 
 app.listen(port, () => {
