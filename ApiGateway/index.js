0a47b67e2223b9740534fd6a1c6b60d1526d156b
Kevin <herrbach.kevin@gmail.com>
2024-01-28 19:10:33 +0900

    Adds example communication between services

--- ApiGateway/index.js
+++ ApiGateway/index.js
@@ -1,9 +1,12 @@
+const { default: axios } = require('axios')
 const express = require('express')
 const app = express()
 const port = 3000
+require('dotenv').config()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  const response = await axios.get(`http://${process.env.AUTH_SERVICE_NAME}:${process.env.AUTH_SERVICE_PORT}/`)
+  res.json(response.data)
 })
 
 app.listen(port, () => {
